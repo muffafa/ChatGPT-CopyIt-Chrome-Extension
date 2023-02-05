73c3e992041c8fbc2319bda454aa95697e4086f9
muffafa <mm.savar@gmail.com>
2023-02-05 14:15:08 +0300

    it is workinggg!!

--- scripts/content.js
+++ scripts/content.js
@@ -1,22 +1,38 @@
-const article = document.querySelector("article");
+const targetNode = document.body;
 
-// `document.querySelector` may return null if the selector doesn't match anything.
-if (article) {
-    const text = article.textContent;
-    const wordMatchRegExp = /[^\s]+/g; // Regular expression
-    const words = text.matchAll(wordMatchRegExp);
-    // matchAll returns an iterator, convert to array to get word count
-    const wordCount = [...words].length;
-    const readingTime = Math.round(wordCount / 200);
-    const badge = document.createElement("p");
-    // Use the same styling as the publish information in an article's header
-    badge.classList.add("color-secondary-text", "type--caption");
-    badge.textContent = `⏱️ ${readingTime} min read`;
-  
-    // Support for API reference docs
-    const heading = article.querySelector("h1");
-    // Support for article docs with date
-    const date = article.querySelector("time")?.parentNode;
-  
-    (date ?? heading).insertAdjacentElement("afterend", badge);
-  }
+const observer = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+        if (mutation.addedNodes.length) {
+            const elements = [...document.querySelectorAll(".text-base")];
+            const filteredElements = [];
+            for (let i = 0; i < elements.length; i++) {
+                if (i % 2 !== 0) {
+                    filteredElements.push(elements[i]);
+                }
+            }
+            filteredElements.forEach((filteredElement) => {
+                const childPElements = filteredElement.getElementsByTagName("p");
+                for (let i = 0; i < childPElements.length; i++) {
+                    if (!childPElements[i].querySelector("button")) {
+                        const copyButton = document.createElement("button");
+                        copyButton.textContent = "+";
+                        copyButton.style.backgroundColor = "lightgray";
+                        copyButton.style.color = "white";
+                        copyButton.style.padding = "5px 10px";
+                        copyButton.style.marginLeft = "8px";
+                        copyButton.style.borderRadius = "5px";
+                        copyButton.style.cursor = "pointer";
+                        copyButton.addEventListener("click", () => {
+                            navigator.clipboard.writeText(childPElements[i].textContent);
+                        });
+                        childPElements[i].appendChild(copyButton);
+                        
+                    }
+                }
+            });
+        }
+    });
+});
+
+const observerConfig = { childList: true, subtree: true };
+observer.observe(targetNode, observerConfig);
