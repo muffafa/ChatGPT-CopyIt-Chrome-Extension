9b3e75632ade25de88ad72765cb68c04e916fc93
muffafa <mm.savar@gmail.com>
2023-02-21 17:10:08 +0300

    copied tooltip 3 second and green button

--- scripts/content.js
+++ scripts/content.js
@@ -1,37 +1,45 @@
 const targetNode = document.body;
 
 const observer = new MutationObserver((mutations) => {
-    mutations.forEach((mutation) => {
-        if (mutation.addedNodes.length) {
-            const elements = [...document.querySelectorAll(".text-base")];
-            const filteredElements = [];
-            for (let i = 0; i < elements.length; i++) {
-                if (i % 2 !== 0) {
-                    filteredElements.push(elements[i]);
-                }
-            }
-            filteredElements.forEach((filteredElement) => {
-                const childPElements = filteredElement.getElementsByTagName("p");
-                const lastPElement = childPElements[childPElements.length - 1]; // get the last paragraph
-                if (!lastPElement.querySelector("button")) { // check if the button is not already added
-                    const copyButton = document.createElement("button");
-                    copyButton.textContent = "+";
-                    copyButton.style.backgroundColor = "lightgray";
-                    copyButton.style.color = "white";
-                    copyButton.style.padding = "5px 10px";
-                    copyButton.style.marginLeft = "8px";
-                    copyButton.style.borderRadius = "5px";
-                    copyButton.style.cursor = "pointer";
-                    copyButton.addEventListener("click", () => {
-                        const paragraphs = filteredElement.getElementsByTagName("p");
-                        const textContent = Array.from(paragraphs).map((p) => p.textContent).join("\n\n");
-                        navigator.clipboard.writeText(textContent);
-                    });
-                    lastPElement.appendChild(copyButton);
-                }
-            });
+  mutations.forEach((mutation) => {
+    if (mutation.addedNodes.length) {
+      const elements = [...document.querySelectorAll(".text-base")];
+      const filteredElements = [];
+      for (let i = 0; i < elements.length; i++) {
+        if (i % 2 !== 0) {
+          filteredElements.push(elements[i]);
         }
-    });
+      }
+      filteredElements.forEach((filteredElement) => {
+        const childPElements = filteredElement.getElementsByTagName("p");
+        const lastPElement = childPElements[childPElements.length - 1]; // get the last paragraph
+        if (!lastPElement.querySelector("button")) { // check if the button is not already added
+          const copyButton = document.createElement("button");
+          copyButton.textContent = "+";
+          copyButton.style.backgroundColor = "lightgray";
+          copyButton.style.color = "white";
+          copyButton.style.padding = "5px 10px";
+          copyButton.style.marginLeft = "8px";
+          copyButton.style.borderRadius = "5px";
+          copyButton.style.cursor = "pointer";
+          copyButton.addEventListener("click", () => {
+            const paragraphs = filteredElement.getElementsByTagName("p");
+            const textContent = Array.from(paragraphs).map((p) => p.textContent).join("\n\n");
+            navigator.clipboard.writeText(textContent);
+            
+            // add copied tooltip and change button color to green
+            copyButton.textContent = "Copied!";
+            copyButton.style.backgroundColor = "green";
+            setTimeout(() => {
+              copyButton.textContent = "+";
+              copyButton.style.backgroundColor = "lightgray";
+            }, 3000);
+          });
+          lastPElement.appendChild(copyButton);
+        }
+      });
+    }
+  });
 });
 
 const observerConfig = { childList: true, subtree: true };
