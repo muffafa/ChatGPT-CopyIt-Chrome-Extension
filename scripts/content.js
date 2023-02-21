2efcf1642547246cfa70bc7e1a40e2dc1d38a538
muffafa <mm.savar@gmail.com>
2023-02-21 17:28:46 +0300

    filteredAnswers renamed to chatGptAnswers

--- scripts/content.js
+++ scripts/content.js
@@ -4,14 +4,14 @@ const observer = new MutationObserver((mutations) => {
   mutations.forEach((mutation) => {
     if (mutation.addedNodes.length) {
       const elements = [...document.querySelectorAll(".text-base")];
-      const filteredElements = [];
+      const chatGptAnswers = [];
       for (let i = 0; i < elements.length; i++) {
         if (i % 2 !== 0) {
-          filteredElements.push(elements[i]);
+          chatGptAnswers.push(elements[i]);
         }
       }
-      filteredElements.forEach((filteredElement) => {
-        const childPElements = filteredElement.getElementsByTagName("p");
+      chatGptAnswers.forEach((chatGptAnswer) => {
+        const childPElements = chatGptAnswer.getElementsByTagName("p");
         const lastPElement = childPElements[childPElements.length - 1]; // get the last paragraph
         if (!lastPElement.querySelector("button")) { // check if the button is not already added
           const copyButton = document.createElement("button");
@@ -23,7 +23,7 @@ const observer = new MutationObserver((mutations) => {
           copyButton.style.borderRadius = "5px";
           copyButton.style.cursor = "pointer";
           copyButton.addEventListener("click", () => {
-            const paragraphs = filteredElement.getElementsByTagName("p");
+            const paragraphs = chatGptAnswer.getElementsByTagName("p");
             const textContent = Array.from(paragraphs).map((p) => p.textContent).join("\n\n");
             navigator.clipboard.writeText(textContent);
             
