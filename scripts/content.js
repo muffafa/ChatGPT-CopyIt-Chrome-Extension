4d7d30598e68c01b48ad04a47dc8e27d00166199
muffafa <mm.savar@gmail.com>
2023-02-21 18:44:50 +0300

    Revert "span doesnt work"
    This reverts commit 6c4f5b4e63cba6a884bafb02fcdc9587dc7e420f.

--- scripts/content.js
+++ scripts/content.js
@@ -16,7 +16,6 @@ const observer = new MutationObserver((mutations) => {
         if (!lastPElement.querySelector("button")) { // check if the button is not already added
           const copyButton = document.createElement("button");
           copyButton.textContent = "+";
-          //copyButton.name = "chatGptCoppyButton";
           copyButton.style.backgroundColor = "lightgray";
           copyButton.style.color = "white";
           copyButton.style.padding = "5px 10px";
@@ -32,11 +31,13 @@ const observer = new MutationObserver((mutations) => {
             copyButton.textContent = "Copied!";
             copyButton.style.backgroundColor = "green";
             setTimeout(() => {
-              copyButton.textContent = "+";
+              copyButton.textContent = "Copy";
               copyButton.style.backgroundColor = "lightgray";
             }, 3000);
           });
-          lastPElement.appendChild(copyButton);
+          const copyButtonContainer = document.createElement("span");
+          copyButtonContainer.appendChild(copyButton);
+          lastPElement.appendChild(copyButtonContainer);
         }
       });
     }
