6c188fac1c7a8e13f7f67f8e04d07c5c1f6981e2
muffafa <mm.savar@gmail.com>
2023-02-22 16:47:30 +0300

    fully cleaned

--- scripts/content.js
+++ scripts/content.js
@@ -1,29 +1,19 @@
-// >> welcome message
-console.log("%c This page is modified by ChatGPT copyit.", "font-size: 16px;");
+const allAnswers = () => [...document.querySelectorAll(".markdown.prose")];
 
-// get all answers
-const allAnswers = () => [...document.querySelectorAll(".markdown.prose")]; //' get all answers //!
-
-// >> starts when page loaded
 window.addEventListener('load', () => {
 
-    /// fixed selectors
-    const messageScrollSection = 'main > .overflow-hidden > div > div';
-
-    //>> set action buttons after nav click
-    // functions set's a click listener on the nav. when it occurs it waits until the messages loaded,
     newChatLoadObserver();
     function newChatLoadObserver() {
         waitForElement('nav a, form textarea', ()=>{
             const navElements = document.querySelectorAll('nav > div a, nav a:nth-child(1)');
             navElements.forEach((navEl)=>{
-                navEl.addEventListener('click', async () => { // nav clicked
+                navEl.addEventListener('click', async () => {
                     await new Promise(r => setTimeout(r, 250));
                     waitForElement('.text-base, main h1', ()=> {
-                        if (document.querySelector('.text-base')) { // chat loaded
+                        if (document.querySelector('.text-base')) {
                             console.log('existing chat loaded');
                             newChatLoaded();
-                        } else if (document.querySelector('main h1')) { // new chat loaded
+                        } else if (document.querySelector('main h1')) {
                             console.log('new chat opened');
                             newChatLoaded();
                         }
@@ -31,68 +21,56 @@ window.addEventListener('load', () => {
                     });
                 });
             });
-        }, 5000, 100, () => {
         });
     }
-    // todo : temp : after several crashes when chat loads
+
     setInterval(()=>{
         newChatLoaded();
     }, 1000);
-    // code that runs when a new chat gets loaded
+
     function newChatLoaded() {
-        
-        waitForElement('main > :nth-child(2) form > :nth-child(1) > :nth-child(2)', (textBox)=>{
-            textBox.click();
-        });
-    
-        try { // update message action elements
+        try {
             updateActionElements();
         } catch (error) {
             console.error('Error in updateActionElements:', error);
         }
+    }
 
-    } // newChatLoaded();
-
-    // improve of message area
     waitForElement('main > div.overflow-hidden', (messageWrapper) => {
-        //>> message send monitoring
         (() => {
             updateActionElements();
             const observer = new MutationObserver(() => {
                 updateActionElements();
             });
             observer.observe(messageWrapper, { childList: true, subtree: true });
-            // >> always start from bottom message
-            const scrollingSection = messageWrapper.firstChild;
-            scrollingSection.scrollTop = scrollingSection.scrollHeight;
         })();
     });
+
     function updateActionElements() {
-        const answers = allAnswers(); // get all answers
-        for (let i = 0; i < answers.length; i += 1) { // apply changes to all the answers
-            const resultElement = answers[i];
+        const answers = allAnswers();
+        answers.forEach((resultElement, i) => {
             actionUpdates(resultElement, i);
-            resultElement.addEventListener("DOMSubtreeModified", () => { // update counter when content changes
-                actionUpdates(resultElement, i);
+            resultElement.addEventListener("DOMSubtreeModified", () => {
+              actionUpdates(resultElement, i);
             });
-        }
+          });
     }
+
     function actionUpdates(resultElement, i) {
         actionContainer(resultElement, i);
         addCopyButtonToResult(resultElement, i);
         updateCounterForResult(resultElement, i);
     }
-    // adds action buttons container
+
     function actionContainer(answer, i) {
-        if (document.querySelector(`#action-buttons-${i}`)) return;  // don't pass if there is already an element with this index
-        let actionContainer = Object.assign(document.createElement('div'), { // create a action button if there isn't one
+        if (document.querySelector(`#action-buttons-${i}`)) return;  
+        let actionContainer = Object.assign(document.createElement('div'), {
             id: `action-buttons-${i}`,
             className: 'gpt-copyit gptc-actions'
         })
         answer.insertAdjacentElement("afterend", actionContainer);
     }
 
-    // create and update the counter for a specific index
     function updateCounterForResult(answer, index) {
         const prevCounter = document.querySelector(`#action-buttons-counter-${index}`);
         const prevCounterText = prevCounter ? prevCounter.innerText : "";
@@ -106,40 +84,34 @@ window.addEventListener('load', () => {
             className: 'gpt-copyit gptc-msg-details'
         })
         if (prevCounterText !== counterElement.textContent) {
-            if (prevCounter) prevCounter.remove(); // if there's already a prev counter remove it
+            if (prevCounter) prevCounter.remove();
             counterElement.id = `action-buttons-counter-${index}`;
             const actionWrapper = document.querySelector(`#action-buttons-${index}`);
             actionWrapper.appendChild(counterElement);
         }
-    } // todo : may be more efficient to just replace the inner instead of creating a new element
+    } 
 
-    // add copy button to answer if it doesn't already has one
     function addCopyButtonToResult(answer, index) {
-        if (document.querySelector(`#result-copy-button-${index}`)) return; // stop if answer already has copy button
-        const actionWrapper = document.querySelector(`#action-buttons-${index}`); // get action wrapper
+        if (document.querySelector(`#result-copy-button-${index}`)) return; 
+        const actionWrapper = document.querySelector(`#action-buttons-${index}`);
 
         const actionButtonWrapper = Object.assign(document.createElement('div'), {
             className: 'gpt-copyit gptc-action-buttons'
         })
 
-        // get element things
         var elementText = () => answer.innerText.replace('Copy code', '');
-        var elementHTML = () => answer.innerHTML;
 
-        // create copy button
         const copyButton = Object.assign(document.createElement('button'), {
             id: `result-copy-button-${index}`,
             className: 'gpt-copyit gptc-button',
             textContent: 'Copy',
             onclick: (e) => {
-                if (e.shiftKey) { // todo : add feature more visible
-                    copyRichText(answer);
-                } else {
-                    navigator.clipboard.writeText(elementText());
-                }
+                navigator.clipboard.writeText(elementText());
                 copyButton.textContent = "Copied!";
+                copyButton.style.backgroundColor = '#00FF00';
                 setTimeout(() => {
                     copyButton.textContent = "Copy";
+                    copyButton.style.backgroundColor = '#343441';
                 }, 1500);
             }
         });
@@ -149,16 +121,6 @@ window.addEventListener('load', () => {
     }
 });
 
-/// general
-// >> copy rich text of element
-const copyRichText = async (element) => {
-    const content = element.innerHTML;
-    const blob = new Blob([content], { type: 'text/html' });
-    const richTextInput = new ClipboardItem({ 'text/html': blob });
-    await navigator.clipboard.write([richTextInput]);
-};
-
-// >> wait for an element to appear and then run a callback function
 function waitForElement(selector, callback, maxWaitTime = 5000, speed = 100, notFoundCallback = null) {
     const endTime = Date.now() + maxWaitTime;
     const interval = setInterval(() => {
